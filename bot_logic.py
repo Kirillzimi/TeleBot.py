84612a8f45c302d15dfb10e098218e4ed9132cea
Kirillzimi <kirill.zimich@adolfinum.de>
2025-01-08 20:24:27 +0100

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,37 +1,27 @@
-import telebot
-from bot_logic import gen_pass, gen_emodji, flip_coin
-  
-bot = telebot.TeleBot("7937749892:AAHwc2TQRhzO5mytinzbCDSV_Sbj2Rv2RVg")
+import random
+import requests
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>123456789"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+    return password
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
     
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
-    
-@bot.message_handler(commands=['hello'])
-def send_hello(message):
-    bot.reply_to(message, "Привет! Как дела?")
-    
-@bot.message_handler(commands=['bye'])
-def send_bye(message):
-    bot.reply_to(message, "Пока! Удачи!")
 
-@bot.message_handler(commands=['random_password'])
-def get_password(message):
-    bot.reply_to(message, gen_pass(10))
-
-@bot.message_handler(commands=['emodji'])
-def send_emodji(message):
-    emodji = gen_emodji()
-    bot.reply_to(message, f"Вот эмоджи': {emodji}")
-
-@bot.message_handler(commands=['coin'])
-def send_coin(message):
-    coin = flip_coin()
-    bot.reply_to(message, f"Монетка выпала так: {coin}")
+def get_duck_image_url():    
+        url = 'https://random.dog/db7ea879-376a-4e00-b4e0-ee5b413be84d.jpg'
+        res = requests.get(url)
+        data = res.json()
+        return data['url']
 
 
 
 
-
-print("BOT started")
-bot.polling()
